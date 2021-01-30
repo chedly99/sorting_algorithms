6045baa3a007ba529d764bb41301116bf009a0e4
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-30 12:08:45 +0100

    bubbles

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+* swap_items - swap_2_integers
+* @x: integer1
+* @y: integer2
+*/
+void swap_items(int *x, int *y)
+{
+int v;
+
+v = *x;
+*x = *y;
+*y = v;
+}
+
+/**
+* bubble_sort - sort integer in print_array
+* @array: array
+* @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+unsigned int i;
+unsigned int j;
+
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - 1 ; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap_items(&array[j], &array[j + 1]);
+print_array(array, size);
+}
+}
+}
+}

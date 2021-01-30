7aae93b5001b2905cb036b231a768c0a492d7fb3
cehdly99 <mohamed.chedliiy@gmail.com>
2021-01-30 12:59:47 +0100

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,6 +23,8 @@ void bubble_sort(int *array, size_t size)
 unsigned int i;
 unsigned int j;
 
+if (array == NULL || size < 2)
+return;
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - 1 ; j++)
